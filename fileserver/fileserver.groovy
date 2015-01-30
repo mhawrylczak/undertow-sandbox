91640445043644d3e07c0731f702978ff1e7fabe
Marek Hawrylczak <wladyslaw.hawrylczak@allegro.pl>
2015-01-30 13:11:27 +0100

    file server sample

---
+++ fileserver/fileserver.groovy
@@ -0,0 +1,11 @@
+@Grab(group = 'io.undertow', module = 'undertow-core', version = '1.1.2.Final')
+import io.undertow.Undertow;
+import io.undertow.server.handlers.resource.FileResourceManager
+
+import static io.undertow.Handlers.resource;
+
+Undertow.builder()
+        .addHttpListener(8080, "localhost")
+        .setHandler(resource(new FileResourceManager(new File(System.getProperty("user.dir")), 100)).setDirectoryListingEnabled(true))
+        .build()
+        .start()
